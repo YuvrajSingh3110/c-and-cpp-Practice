d029ae9784d5dab4782c2297727129a6a4cf32c1
Yuvraj Singh <ysrishu1031@gmail.com>
2023-06-20 16:19:47 +0530

    making a POST request in golang

--- Golang/20webReqVerb/main.go
+++ Golang/20webReqVerb/main.go
@@ -8,28 +8,54 @@ import (
 )
 
 func main() {
-	PerformGetRequest()
+	//PerformGetRequest()
+	PerformPostJsonRequest()
 }
-func PerformGetRequest() {
-	const myUrl = "http://localhost:8000/get"
+// func PerformGetRequest() {
+// 	// const myUrl = "http://localhost:8000/get"
 
-	response, err := http.Get(myUrl)
+// 	// response, err := http.Get(myUrl)
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
+
+// 	// defer response.Body.Close()
+
+// 	// fmt.Println("Status code: ", response.StatusCode)
+// 	// fmt.Println("Content length: ", response.ContentLength)
+
+// 	// var responseString strings.Builder
+// 	// //alternatively we can also use strings library which is considered very strong
+// 	// content, _ := ioutil.ReadAll(response.Body)
+// 	// byteCnt ,_ := responseString.Write(content) 
+// 	// //through responseString we are having the raw data all the time which is very useful
+// 	// fmt.Println("Byte count: ",byteCnt)
+// 	// fmt.Println(responseString.String())
+
+// 	// //println(string(content))
+// }
+
+func PerformPostJsonRequest(){
+	const myUrl = "http://localhost:8000/post"
+
+	//fake json payload
+	request := strings.NewReader(`
+		{
+			"coursename":"get going with golang",
+			"price":0,
+		}
+	`)
+
+	response, err := http.Post(myUrl, "application/json", request)
 	if err != nil {
 		panic(err)
 	}
 
 	defer response.Body.Close()
 
-	fmt.Println("Status code: ", response.StatusCode)
-	fmt.Println("Content length: ", response.ContentLength)
-
-	var responseString strings.Builder
-	//alternatively we can also use strings library which is considered very strong
-	content, _ := ioutil.ReadAll(response.Body)
-	byteCnt ,_ := responseString.Write(content) 
-	//through responseString we are having the raw data all the time which is very useful
-	fmt.Println("Byte count: ",byteCnt)
-	fmt.Println(responseString.String())
-
-	//println(string(content))
+	content, err:= ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(content))
 }
