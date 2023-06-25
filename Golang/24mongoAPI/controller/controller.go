8e89fb959f9fa647b1baf9d603eb8580eb653430
Yuvraj Singh <ysrishu1031@gmail.com>
2023-06-25 19:03:08 +0530

    get all collection in mongodb in golang

--- Golang/24mongoAPI/controller/controller.go
+++ Golang/24mongoAPI/controller/controller.go
@@ -21,14 +21,14 @@ var collection *mongo.Collection
 
 //connect with mongoDB
 
-func init()  {
+func init() {
 	//client option
 	clientOption := options.Client().ApplyURI(connectionString)
 
 	//connect to mongoDB
 	client, err := mongo.Connect(context.TODO(), clientOption)
 
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Successfully connected to MongoDB")
@@ -40,21 +40,21 @@ func init()  {
 
 //MongoDB helpers - file
 
-//insert 1 record
-func insertOneMovie(movie model.Netflix){
+// insert 1 record
+func insertOneMovie(movie model.Netflix) {
 	inserted, err := collection.InsertOne(context.Background(), movie)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Inserted one movie in db with id: ", inserted.InsertedID)
 }
 
-//update 1 record
-func updateOneMovie(movieId string)  {
-	id, _ :=primitive.ObjectIDFromHex(movieId)
-	filter := bson.M{"_id": id}   //bson.M is a non-ordered representaion
+// update 1 record
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id} //bson.M is a non-ordered representaion
 	update := bson.M{"$set": bson.M{"watched": true}}
-	
+
 	result, err := collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		log.Fatal(err)
@@ -62,8 +62,8 @@ func updateOneMovie(movieId string)  {
 	fmt.Println("Modified count: ", result.ModifiedCount)
 }
 
-//delete 1 record
-func deleteOneMovie(movieId string)  {
+// delete 1 record
+func deleteOneMovie(movieId string) {
 	id, _ := primitive.ObjectIDFromHex(movieId)
 	filter := bson.M{"_id": id}
 
@@ -71,15 +71,35 @@ func deleteOneMovie(movieId string)  {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Movie got deleted with delete count: ",deleteCount)
+	fmt.Println("Movie got deleted with delete count: ", deleteCount)
 }
 
-//delete all records
-func deleteAllMovie()  {
+// delete all records
+func deleteAllMovie() int64{
 	//bson.D is an ordered representaion. {} means no id is given and everything is selected for deletion
 	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Records deleted with delete count: ", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+}
+
+// get all movies from db
+func getAllMovies() []primitive.M {
+	curr, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var movies []primitive.M
+	for curr.Next(context.Background()) {
+		var movie bson.M
+		err := curr.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+	defer curr.Close(context.Background())
+	return movies
 }
